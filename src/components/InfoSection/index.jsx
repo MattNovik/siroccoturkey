c1f6877e40a0c7b26ad8826fb1b16644001ed992
lockdur <matvej.novik@gmail.com>
2023-06-02 13:31:41 +0300

    ;fsdg

--- src/components/InfoSection/index.jsx
+++ src/components/InfoSection/index.jsx
@@ -3,9 +3,9 @@ import { nanoid } from 'nanoid';
 import AdditionalInfoBlock from '../AdditionalInfoBlock';
 import addInfo1 from '../../assets/addInfo1.png';
 import addInfo2 from '../../assets/addinfo2.jpg';
-import addInfo3 from '../../assets/addInfo6.jpg';
+import addInfo3 from '../../assets/addInfo6.png';
 import addInfo5 from '../../assets/addInfo5.png';
-import addInfo6 from '../../assets/addInfo3.jpg';
+import addInfo6 from '../../assets/addInfo3.png';
 import addInfo7 from '../../assets/addinfo7.png';
 
 const InfoSection = ({ data }) => {
