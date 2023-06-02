0936dc37834bb91f153bf97cf03dfb961d49de8d
lockdur <matvej.novik@gmail.com>
2023-06-02 12:09:39 +0300

    fix;

--- src/components/InfoSection/index.jsx
+++ src/components/InfoSection/index.jsx
@@ -1,13 +1,13 @@
 import './index.scss';
 import { nanoid } from 'nanoid';
 import AdditionalInfoBlock from '../AdditionalInfoBlock';
-import addInfo1 from '../../assets/addinfo1.png';
-import addInfo2 from '../../assets/addinfo2.jpg';
-import addInfo3 from '../../assets/addinfo3.jpg';
-import addInfo4 from '../../assets/addinfo4.jpg';
-import addInfo5 from '../../assets/addinfo5.png';
-import addInfo6 from '../../assets/addinfo6.jpg';
-import addInfo7 from '../../assets/addinfo7.png';
+import addInfo1 from '../../assets/addInfo1.png';
+import addInfo2 from '../../assets/addInfo2.jpg';
+import addInfo3 from '../../assets/addInfo3.jpg';
+import addInfo4 from '../../assets/addInfo4.jpg';
+import addInfo5 from '../../assets/addInfo5.png';
+import addInfo6 from '../../assets/addInfo6.jpg';
+import addInfo7 from '../../assets/addInfo7.png';
 
 const InfoSection = ({ data }) => {
   const dataList = data.additionalInfo;
